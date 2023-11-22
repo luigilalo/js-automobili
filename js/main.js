3d27c58a7111ea8668fa6b0c7d59e4a056104d87
luigilalo <luigilaloe1@gmail.com>
2023-11-22 16:31:00 +0100

    First Array created

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let automobili = [
+    {marca: "FIAT", model: 10, engine: "diesel"},
+    {marca: 'Audi', model: 13, engine: "diesel"},
+    {marca: 'BMW', model: 4, engine: "gpl"},
+    {marca: 'Lotus', model: 11, engine: "gpl"},
+    {marca: 'FIAT', model: 2, engine: "gas"},
+    {marca: 'Nissan', model: 5, engine: "electric"},
+    {marca: 'Mazda', model: 7, engine: "electric"},
+    {marca: 'FIAT', model: 3, engine: "gas"},
+    {marca: 'Ford', model: 6, engine: "gas"},
+];
