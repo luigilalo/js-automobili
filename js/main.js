b4b6402ffa22b6baf4c61ad029768f690af2f2a2
luigilalo <luigilaloe1@gmail.com>
2023-11-22 17:02:18 +0100

    Exercise Done, doing bonus

--- js/main.js
+++ js/main.js
@@ -7,5 +7,24 @@ let automobili = [
     {marca: 'Nissan', model: 5, engine: "electric"},
     {marca: 'Mazda', model: 7, engine: "electric"},
     {marca: 'FIAT', model: 3, engine: "gas"},
-    {marca: 'Ford', model: 6, engine: "gas"},
+    {marca: 'Ford', model: 6, engine: "hybrid"},
 ];
+
+let dieselCars = [];
+let reaminingCars = [];
+let gplCars = [];
+
+automobili.forEach(function(element) {
+if (element.engine == "gpl") {
+    gplCars.push(element)
+}
+else if (element.engine == "diesel") {
+    dieselCars.push(element)
+}
+else {reaminingCars.push(element)}
+}
+
+)
+console.log("Queste sono le macchine Diesel" , dieselCars)
+console.log("Queste sono le macchine restanti" , reaminingCars)
+console.log("Queste sono le macchine GPL", gplCars)
